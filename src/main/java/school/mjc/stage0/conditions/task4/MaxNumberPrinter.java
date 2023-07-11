99a04b2fbabbc457f7526df7f76b83d31da00f39
Sergey Tsoy <sergeytsoy740@gmail.com>
2023-07-11 15:08:11 +0600

    Module #4.1. Task #4. Ternary Conditions

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int max = 0;
+
+        if (first >= second) max = first;
+        else  max = second;
+
+        if (third >= max) max = third;
+
+        System.out.println(max);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -1,6 +1,14 @@
 package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
-    public void printGreatest(int first, int second, int third) {
+    public static void main(String[] args) {
+        printGreatest(12, 1, 10);
+    }
+    public static void printGreatest(int first, int second, int third) {
+        int max = 0;
+
+        max = third >= (max = first >= second ? first : second) ? third : max;
+
+        System.out.println(max);
     }
 }
